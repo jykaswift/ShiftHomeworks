ca0b056860398fee45710cdb97621c63f088f968
Eugene <jykaswift@yandex.ru>
2024-04-13 09:07:40 +0300

    рефактор: удалил неиспользуемые методы в appDelegate

--- Homework1/Homework1/AppDelegate.swift
+++ Homework1/Homework1/AppDelegate.swift
@@ -1,28 +1,11 @@
 //
 //  AppDelegate.swift
-//  AutomobileAccounting
+//  Homework1
 //
-//  Created by Евгений Борисов on 09.04.2024.
+//  Created by Евгений Борисов on 13.04.2024.
 //
 
 import UIKit
 
 @main
-class AppDelegate: UIResponder, UIApplicationDelegate {
-    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        return true
-    }
-
-    func application(
-        _ application: UIApplication,
-        configurationForConnecting connectingSceneSession: UISceneSession,
-        options: UIScene.ConnectionOptions
-    ) -> UISceneConfiguration {
-        return UISceneConfiguration(name: "Default Configuration", sessionRole: connectingSceneSession.role)
-    }
-
-    func application(_ application: UIApplication, didDiscardSceneSessions sceneSessions: Set<UISceneSession>) {
-    }
-
-}
-
+class AppDelegate: UIResponder, UIApplicationDelegate { }
