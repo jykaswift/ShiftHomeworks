e1489c04dbcc9176a1b3c814697c49018cf229dc
Eugene <jykaswift@yandex.ru>
2024-05-12 16:29:53 +0300

    рефактор: убрал форс анврап с функции генерации случайных строк

--- Homework4/Homework4/Model/MockData.swift
+++ Homework4/Homework4/Model/MockData.swift
@@ -84,6 +84,6 @@ class MockData {
 
     static func getRandomString(length: Int) -> String {
       let letters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
-      return String((0..<length).map { _ in letters.randomElement()! })
+      return String((0..<length).map { _ in letters.randomElement() ?? "а" })
     }
 }
